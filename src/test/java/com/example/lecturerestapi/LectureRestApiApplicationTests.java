14130e90d9da4dabaf2b3e83fe33284063f556da
Davin <85967571+dav1n9@users.noreply.github.com>
2024-07-08 13:10:58 +0900

    init commit

---
+++ src/test/java/com/example/lecturerestapi/LectureRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
