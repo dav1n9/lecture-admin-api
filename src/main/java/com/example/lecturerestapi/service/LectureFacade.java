ea3d61b7201b534312acb28e40cf75d612188f36
Davin <85967571+dav1n9@users.noreply.github.com>
2024-07-10 12:33:42 +0900

    refactor: 관리자 검증 메소드 분리

--- src/main/java/com/example/lecturerestapi/service/LectureFacade.java
+++ src/main/java/com/example/lecturerestapi/service/LectureFacade.java
@@ -1,7 +1,6 @@
 package com.example.lecturerestapi.service;
 
 import com.example.lecturerestapi.entity.Admin;
-import com.example.lecturerestapi.entity.AdminRole;
 import com.example.lecturerestapi.entity.Category;
 import com.example.lecturerestapi.dto.LectureRequest;
 import com.example.lecturerestapi.dto.LectureResponse;
@@ -18,38 +17,28 @@ public class LectureFacade {
     private final TeacherService teacherService;
 
     public LectureResponse save(LectureRequest request, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER || admin.getRole() == AdminRole.STAFF) {
-            Teacher teacher = teacherService.findTeacherById(request.getTeacherId());
-            return lectureService.save(request, teacher);
-        }
-        throw new IllegalArgumentException("관리자만 강사 등록이 가능합니다.");
+        teacherService.validateAdmin(admin);
+        Teacher teacher = teacherService.findTeacherById(request.getTeacherId());
+        return lectureService.save(request, teacher);
     }
 
     public LectureResponse update(Long lectureId, LectureRequest request, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER) {
-            return lectureService.update(lectureId, request);
-        }
-        throw new IllegalArgumentException("MANAGER 만 강사 수정이 가능합니다.");
+        teacherService.validateManager(admin);
+        return lectureService.update(lectureId, request);
     }
 
     public LectureResponse findById(Long lectureId, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER || admin.getRole() == AdminRole.STAFF) {
-            return lectureService.findById(lectureId);
-        }
-        throw new IllegalArgumentException("관리자만 강사 조회가 가능합니다.");
+        teacherService.validateAdmin(admin);
+        return lectureService.findById(lectureId);
     }
 
     public List<LectureResponse> findByCategory(Category category, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER || admin.getRole() == AdminRole.STAFF) {
-            return lectureService.findByCategory(category);
-        }
-        throw new IllegalArgumentException("관리자만 강사 조회가 가능합니다.");
+        teacherService.validateAdmin(admin);
+        return lectureService.findByCategory(category);
     }
 
     public Long delete(Long lectureId, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER) {
-            return lectureService.delete(lectureId);
-        }
-        throw new IllegalArgumentException("MANAGER 만 강사 삭제가 가능합니다.");
+        teacherService.validateManager(admin);
+        return lectureService.delete(lectureId);
     }
 }
--- src/main/java/com/example/lecturerestapi/service/TeacherService.java
+++ src/main/java/com/example/lecturerestapi/service/TeacherService.java
@@ -5,6 +5,7 @@ import com.example.lecturerestapi.dto.TeacherResponse;
 import com.example.lecturerestapi.entity.Admin;
 import com.example.lecturerestapi.entity.AdminRole;
 import com.example.lecturerestapi.entity.Teacher;
+import com.example.lecturerestapi.exception.ErrorType;
 import com.example.lecturerestapi.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,39 +17,43 @@ public class TeacherService {
     private final TeacherRepository teacherRepository;
 
     public TeacherResponse save(TeacherRequest request, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER || admin.getRole() == AdminRole.STAFF)
-            return new TeacherResponse(teacherRepository.save(request.toEntity()));
-        throw new IllegalArgumentException("관리자만 강사 등록이 가능합니다.");
+        validateAdmin(admin);
+        return new TeacherResponse(teacherRepository.save(request.toEntity()));
     }
 
     @Transactional
     public TeacherResponse update(Long teacherId, TeacherRequest request, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER) {
-            Teacher teacher = findTeacherById(teacherId);
-            teacher.update(request);
-            return new TeacherResponse(teacher);
-        }
-        throw new IllegalArgumentException("MANAGER 만 강사 수정이 가능합니다.");
+        validateManager(admin);
+        Teacher teacher = findTeacherById(teacherId);
+        teacher.update(request);
+        return new TeacherResponse(teacher);
     }
 
     public TeacherResponse findById(Long teacherId, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER || admin.getRole() == AdminRole.STAFF) {
-            Teacher teacher = findTeacherById(teacherId);
-            return new TeacherResponse(teacher);
-        }
-        throw new IllegalArgumentException("관리자만 강사 조회가 가능합니다.");
+        validateAdmin(admin);
+        Teacher teacher = findTeacherById(teacherId);
+        return new TeacherResponse(teacher);
     }
 
     public Long delete(Long teacherId, Admin admin) {
-        if (admin.getRole() == AdminRole.MANAGER) {
-            Teacher teacher = findTeacherById(teacherId);
-            teacherRepository.deleteById(teacher.getId());
-            return teacher.getId();
-        }
-        throw new IllegalArgumentException("MANAGER 만 강사 삭제가 가능합니다.");
+        validateManager(admin);
+        Teacher teacher = findTeacherById(teacherId);
+        teacherRepository.deleteById(teacher.getId());
+        return teacher.getId();
     }
 
     public Teacher findTeacherById(Long teacherId) {
-        return teacherRepository.findById(teacherId).orElseThrow(NullPointerException::new);
+        return teacherRepository.findById(teacherId)
+                .orElseThrow(() -> new NullPointerException(ErrorType.NOT_FOUND_TEACHER.getMessage()));
+    }
+
+    public void validateAdmin(Admin admin) {
+        if (admin.getRole() != AdminRole.MANAGER && admin.getRole() != AdminRole.STAFF)
+            throw new IllegalArgumentException(ErrorType.ACCESS_ADMIN_ONLY.getMessage());
+    }
+
+    public void validateManager(Admin admin) {
+        if (admin.getRole() != AdminRole.MANAGER)
+            throw new IllegalArgumentException(ErrorType.ACCESS_MANAGER_ONLY.getMessage());
     }
 }
